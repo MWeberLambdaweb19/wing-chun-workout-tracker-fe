d0afa7c0d65829b359b50063fc09831699768dbf
MWeberLambdaweb19 <mack.webb37@gmail.com>
2021-01-06 01:54:16 -0800

    Very basic skeleton added

--- wc-tracker/src/App.js
+++ wc-tracker/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 
+// For right now, the entire application will be "single-page"
+// this means that I am coding and designing the entire app on one page, in one functional component
+// until I break things apart. What can go wrong? 
+
+
 function App() {
   return (
     <div>
-      <header>
-      <div id="sample-pfp"></div>
+    {/* WORKOUT PAGE */}
+    <header>
+     <div id="sample-pfp"></div>
       <h1>My Workouts</h1>
       </header>
       <div class="workouts-list">
@@ -23,6 +29,31 @@ function App() {
           <h3>January 2nd, 2021</h3>
         </div>
       </div>
+
+      {/* PROFILE PAGE PAGE */}
+      <header>
+        <h1>My Profile</h1>
+      </header>
+        <div>
+        <p>Name</p>
+        <p>Email</p>
+        <p>Rank</p>
+        <p>Profile Picture:
+          <div id="sample-pfp"></div>
+        </p>
+        </div>
+
+    {/* WELCOME/HOME PAGE */}
+    <header>
+      <h1>Welcome!</h1>
+    </header>
+    <div>
+      <h2>Wing Chun Workout Tracker Version 0.0.1</h2>
+      <h3>Recent News and Updates:</h3>
+      <ul>
+        <li>Created a skeleton of the website</li>
+      </ul>
+    </div>
     </div>
   );
 }
