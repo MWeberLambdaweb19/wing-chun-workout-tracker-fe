214c48ef575f8f9a14053a919844e239276f44a3
MWeberLambdaweb19 <mack.webb37@gmail.com>
2021-01-11 23:53:18 -0800

    Skeleton

---
+++ wc-tracker/src/components/users/login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Login() {
+    return(
+        <div>
+            <form>
+                <input></input>
+                <button></button>
+            </form>
+        </div>
+    )
+}
+
+export default Login;
