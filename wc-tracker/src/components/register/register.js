ba80e491bc3ff02fe3e8ad3d9cb77582811e04f2
MWeberLambdaweb19 <mack.webb37@gmail.com>
2021-02-09 07:41:13 -0800

    Rewriting register form with hooks

--- wc-tracker/src/components/register/register.js
+++ wc-tracker/src/components/register/register.js
@@ -1,70 +1,31 @@
-import React from 'react';
-import SignedUp from './signedup';
+import { useState } from "react";
+import React from "react";
+import { Button } from "@material-ui/core";
 
-class Register extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state={
-            email:"",
-            first:"",
-            last:"",
-            username:"",
-            password:"",
-            verifypassword:""
-        }
-        
+function Register() {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [password, setPassword] = useState("");
+  const [verifyPassword, setVerifyPassword] = useState("");
 
-        this.handleChanges = (event) => {
-            const target = event.target.name
-            this.setState({[target]: event.target.value})
-            // DO NOT FORGET: [] AROUND A VARIABLE MAKE IT A DYNAMIC KEY
-        }
-
-        // You need this if you don't write the function in the constructor
-        this.handleSubmit = this.handleSubmit.bind(this)
-        this.passwordAuth = this.passwordAuth(this)
-    }
-    
-        handleSubmit(event) {
-            this.passwordAuth(event, this.state.password, this.state.verifypassword)
-            alert('Thank you for registering! ' + this.state.username)
+  return (
+    <div>
+      <form>
+        <input
+          name="email"
+          type="email"
+          label="Email"
+          onChange={(event) => {
             event.preventDefault();
-        }
-
-        passwordAuth(event, pass, ver) {
-            event.preventDefault();
-            if (pass === ver) {
-                return
-            } else if (pass !== ver) {
-                alert('Your passwords do not match!')
-                return
-            }
-        }
-
-    render() {
-        return(
-            <div>
-                <p>*Must be filled</p>
-                <p>**Your email is not shared, sold, or otherwise distributed</p>
-                <form onSubmit={() => this.handleSubmit()}>
-                    <label for="email">Email* **</label>
-                    <input name="email" type="email" value={this.state.value} onChange={this.handleChanges}></input>
-                    <label for="first">First Name</label>
-                    <input name="first" type="text" value={this.state.value} onChange={this.handleChanges}></input>
-                    <label for="last">Last Name (Or Initial)</label>
-                    <input name="last" type="text" value={this.state.value} onChange={this.handleChanges}></input>
-                    <label for="username">Username*</label>
-                    <input name="username" value={this.state.value} onChange={this.handleChanges} type="text"></input>
-                    <label for="password">Password</label>
-                    <input name="password" type="password" value={this.state.value} onChange={this.handleChanges}></input>
-                    <label for="verify password">Verify Password</label>
-                    <input name="verify password" type="password" value={this.state.value} onChange={this.handleChanges}></input>
-                    <input type="submit" name="submit" value="Submit"></input>
-                </form>
-            <SignedUp data={this.state} />
-            </div>
-        )
-    }
+            setEmail;
+          }}
+        />
+        <Button color="primary">SCatmit</Button>
+      </form>
+    </div>
+  );
 }
 
 export default Register;
