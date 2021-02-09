7c556b5dcf3c2b974a5563bc34657d47683f3607
MWeberLambdaweb19 <mack.webb37@gmail.com>
2021-02-09 11:53:09 -0800

    Learning to rewrite login statements

--- wc-tracker/src/components/auth/login.js
+++ wc-tracker/src/components/auth/login.js
@@ -1,14 +1,42 @@
-import React from 'react';
+import { logins } from "../dummyData/dummyData.js";
+import { useState } from "react";
 
-function Register() {
-    return(
-        <div>
-            <form onSubmit="submit">
-                <input></input>
-                <button>Submit</button>
-            </form>
-        </div>
-    )
+function Login() {
+  const [emailUser, setEmailUser] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit() {
+    if (true) {
+      if (emailUser == logins["email"] || emailUser == logins["user]"]) {
+        return true;
+      }
+      if (password == logins["password"]) {
+        return true;
+      } else {
+        return false;
+      }
+      console.log("You have logged in!");
+    } else {
+    }
+  }
+  return (
+    <div>
+      <form>
+        <label htmlFor="emailUsername">Your Email or Username</label>
+        <input
+          name="emailUsername"
+          placeholder="Your email or username"
+          onChange={(e) => setEmailUser(e.target.value)}
+        />
+        <label htmlFor="password">Your Password</label>
+        <input
+          name="password"
+          placeholder="Your password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </form>
+    </div>
+  );
 }
 
-export default Register;
+export default Login;
