c0ca6c82901c9c3ba2d869755b3c3df9697307d8
MWeberLambdaweb19 <mack.webb37@gmail.com>
2021-02-15 16:51:25 -0800

    Started work on printing exercises list

---
+++ wc-tracker/src/components/exercises/exercises-list.js
@@ -0,0 +1,12 @@
+import {
+    group1,
+    group2,
+    group3,
+    group4,
+    group5
+  } from "../dummyData/dummyData.js";
+  
+  function ExercisesList() {}
+  
+  export default ExercisesList;
+  
