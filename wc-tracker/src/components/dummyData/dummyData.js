e787bd0469b0ad34d28276f16f0cb59c265e723e
MWeberLambdaweb19 <mack.webb37@gmail.com>
2021-02-15 16:32:57 -0800

    Updated dummyData with dummy data

--- wc-tracker/src/components/dummyData/dummyData.js
+++ wc-tracker/src/components/dummyData/dummyData.js
@@ -2,6 +2,70 @@ let logins = {
     cat: "cayt",
     cayt: "cat"
   };
+
+  let group1 = [
+    "single punch",
+    "double punch",
+    "triple punch",
+    "five punches",
+    "tan dar",
+    "jut dar",
+    "gan dar"
+  ];
   
-  export { logins };
+  let group2 = [
+    "step pak dar",
+    "step pak dar + press and punch",
+    "step tan",
+    "step tan dar",
+    "step tan + double punch",
+    "step tan dar + double punch",
+    "step double punch",
+    "step double punch + double punch",
+    "jit tek",
+    "jit tek + side kick",
+    "jit tek + side kick + press and punch",
+    "jit tek + side kick + press and punch + double punch"
+  ];
+  
+  let group3 = [
+    "med-low bong sao",
+    "med-low bong sao + tan and strike",
+    "punch + med-low bong sao",
+    "punch + med-low bong sao + press and punch",
+    "kwan sao, kwan sao, cheng choi",
+    "high bong sao",
+    "high bong sao + lop and chop",
+    "high bong sao + lop and chop + jut and strike",
+    "punch + high bong sao + lop and chop",
+    "punch + high bong sao + lop and chop + just and strike"
+  ];
+  
+  let group4 = [
+    "jum sao",
+    "jum sao + press and punch",
+    "jum sao + press and punch + double punch",
+    "arrow punch",
+    "arrow punch + press and punch",
+    "arrow punch + press and punch + double punch",
+    "sidestep tan sao",
+    "sidestep tan sao + jit tek",
+    "sidestep tan sao + jit tek + side kick",
+    "sidestep tan sao + jit tek + side kick + press and punch",
+    "sidestep tan sao + jit tek + side kick + press and punch + double punch",
+    "sidestep gan sao",
+    "sidestep gan sao + press and punch",
+    "sidestep gan sao + press and punch + double punch"
+  ];
+  
+  let group5 = [
+    "horse stance punch",
+    "horse stance triple punch",
+    "horse stance + press and punch",
+    "kao sao",
+    "step shoot and push",
+    "step shoot and push + double punch"
+  ];
+  
+  export { logins, group1, group2, group3, group4, group5 };
   
