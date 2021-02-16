45c683a2725dc7acd90fdc934894046073cbeb32
MWeberLambdaweb19 <mack.webb37@gmail.com>
2021-02-15 18:00:47 -0800

    Updated exercises list, working on accordion menus

--- wc-tracker/src/components/exercises/exercises-list.js
+++ wc-tracker/src/components/exercises/exercises-list.js
@@ -6,7 +6,63 @@ import {
     group5
   } from "../dummyData/dummyData.js";
   
-  function ExercisesList() {}
+  function ExercisesList() {
+    return (
+      <div>
+        <div>
+          <p>
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum."
+          </p>
+        </div>
+        <h3 class="accordion">
+          Group 1 <span class="accordion-left">v</span>
+        </h3>
+        <div class="panel">
+          {group1.map((item) => (
+            <p>{item}</p>
+          ))}
+        </div>
+        <h3 class="accordion">
+          Group 2 <span class="accordion-left">v</span>
+        </h3>
+        <div class="panel">
+          {group2.map((item) => (
+            <p>{item}</p>
+          ))}
+        </div>
+        <h3 class="accordion">
+          Group 3 <span class="accordion-left">v</span>
+        </h3>
+        <div class="panel">
+          {group3.map((item) => (
+            <p>{item}</p>
+          ))}
+        </div>
+        <h3 class="accordion">
+          Group 4 <span class="accordion-left">v</span>
+        </h3>
+        <div class="panel">
+          {group4.map((item) => (
+            <p>{item}</p>
+          ))}
+        </div>
+        <h3 class="accordion">
+          Group 5 <span class="accordion-left">v</span>
+        </h3>
+        <div class="panel">
+          {group5.map((item) => (
+            <p>{item}</p>
+          ))}
+        </div>
+      </div>
+    );
+  }
   
   export default ExercisesList;
   
