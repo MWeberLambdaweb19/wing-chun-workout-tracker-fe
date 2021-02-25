6d3d50eb804bf5bb70198c3c1d3e10eb4989bc04
MWeberLambdaweb19 <mack.webb37@gmail.com>
2021-02-24 21:05:13 -0800

    Updating navbar

--- wc-tracker/src/App.js
+++ wc-tracker/src/App.js
@@ -1,18 +1,60 @@
 import "./styles.css";
 import Register from "./components/auth/register";
 import Login from "./components/auth/login";
+import ExercisesList from "./components/exercises/exercises-list.js";
+import { makeStyles } from "@material-ui/core/styles";
+import Link from "@material-ui/core/Link";
+import Typography from "@material-ui/core/Typography";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Wing Chun Workout Tracker</h1>
       <h2>Welcome to our Home Page!</h2>
+      <nav className="Navbar">
+        <Link href="#" className="Link">
+          Link
+        </Link>
+        <Link href="#" className="Link">
+          Link
+        </Link>
+        <Link href="#" className="Link">
+          Link
+        </Link>
+        <Link href="#" className="Link">
+          Link
+        </Link>
+      </nav>
 
       {/* <Register /> */}
-      <Login />
+      {/* <Login /> */}
+      <ExercisesList />
     </div>
   );
 }
+
+
+
+
+
+
+
+
+// import "./styles.css";
+// import Register from "./components/auth/register";
+// import Login from "./components/auth/login";
+
+// export default function App() {
+//   return (
+//     <div className="App">
+//       <h1>Wing Chun Workout Tracker</h1>
+//       <h2>Welcome to our Home Page!</h2>
+
+//       {/* <Register /> */}
+//       <Login />
+//     </div>
+//   );
+// }
 
 
 // BELOW IS LEGACY CODE
